f38bfec93ce5a9b4c06407e9a55b696067c228c5
victoria.sidorchik <victoria.sidorchik@ox.security>
2025-07-01 17:42:03 +0300

    Edit short secret

--- secrets.js
+++ secrets.js
@@ -1,3 +1,3 @@
 export const GH_PAT = "ghp_jrgoU8ts3fbszM5Ln2oH97FGiDX7IU20w4ZN";
 export const LongPassword = "T0m3rS3g3v";
-export const ShortPassword = "T0m3";
+export const ShortPassword = "T0m315";
